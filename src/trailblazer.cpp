cc3421e8253d91ce7c3083de13b0da49d2c71962
Apurba <apurba10024@gmail.com>
2019-04-05 22:46:17 +0600

    1.1 Added Solution to BFS

--- src/trailblazer.cpp
+++ src/trailblazer.cpp
@@ -8,6 +8,8 @@
 #include "set.h"
 #include "pqueue.h"
 #include "point.h"
+#include "map.h"
+#include "stack.h"
 
 using namespace std;
 
@@ -16,15 +18,70 @@ typedef Vector<Vertex*> Path;
 
 // the minimum difference for an alternate route
 const double SUFFICIENT_DIFFERENCE = 0.2;
+Path buildPath(Map<string, Vertex*>& predecesorMap,Vertex* start ,Vertex* end);
+
 
 Path breadthFirstSearch(RoadGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
-    Path emptyPath;
-    return emptyPath;
+    Queue<Vertex*> queue;
+    Set<Vertex*> visitedNeighbors;
+    Map<string, Vertex*> predecesorMap;
+
+    predecesorMap.add(start->name, start);
+    queue.enqueue(start);
+
+    while(!queue.isEmpty()){
+        Vertex* currentVertex = queue.dequeue();// visiting the first vertex in the queue
+        if(currentVertex == end) break; // when we reach our destination index, break the loop
+
+        if(!visitedNeighbors.contains(currentVertex)){ // is this vertex visited before?
+            visitedNeighbors.add(currentVertex);// make it visited
+            currentVertex->setColor(GREEN); // set its color to be green
+
+            Set<Vertex*> neighbors = graph.getNeighbors(currentVertex); // find all neighbors of the current vertex
+            for(Vertex* curNeighbor : neighbors){
+                curNeighbor->setColor(YELLOW); // mark each neighbor to be yellow
+                if(!visitedNeighbors.contains(curNeighbor)){ // is this neighbor visited before?
+                    queue.enqueue(curNeighbor); // if not then add this neighbor to the queue
+                    if(!predecesorMap.containsKey(curNeighbor->name)){ // is this neighbor already has a predecessor?
+                        predecesorMap.add(curNeighbor->name, currentVertex); // if not make the current vertex to its precessor
+                    }
+                }
+            }
+        }
+    }
+    cout << "****** Complted " << endl;
+    return buildPath(predecesorMap,start ,end);
 }
+
+Path buildPath(Map<string, Vertex*>& predecesorMap,Vertex* start ,Vertex* end){
+    Stack<Vertex*> pathStack;
+    Path path;
+    pathStack.push(end);
+    Vertex* predecesor = predecesorMap.get(end->name);
+    while(true){
+        string predecesorName = predecesor->name;
+        pathStack.push(predecesor);
+        predecesor = predecesorMap.get(predecesorName);
+        if(predecesor == start){
+            pathStack.push(start);
+            break;
+        }
+    }
+    while (!pathStack.isEmpty()) {
+        Vertex* pathNode = pathStack.pop();
+        cout << "** Path = " << pathNode->name << endl;
+        path.add(pathNode);
+    }
+    return path;
+}
+
+
+
+
+
+
+
+
 
 Path dijkstrasAlgorithm(RoadGraph& graph, Vertex* start, Vertex* end) {
     // TODO: implement this function; remove these comments
